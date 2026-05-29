94801c2355cf3c10cc8fd52fbbeead57786b0b30
DevanshGupta-6 <contest112010@gmail.com>
2026-05-29 19:19:55 +0530

    Cleaned up the code

--- ArmstrongRange.cpp
+++ ArmstrongRange.cpp
@@ -2,24 +2,29 @@
 #include <cmath>
 using namespace std;
 
+bool isArmstrong(int n){
+    int n1 = n, c = 0, s = 0;
+    while(n>0){
+        c++;
+        n/=10;
+    }
+    n = n1;
+    while(n>0){
+        int d = n%10;
+        s+=pow(d, c);
+        n/=10;
+    }
+    if(s == n1)
+        return true;
+    else
+        return false;
+}
 int main(){
     int n;
     cout<<"Enter the end term: ";
     cin>>n;
     for(int i = 1; i<=n; i++){
-        int i1 = i, s = 0, c = 0;
-        while(i1>0){
-            c++;
-            i1/=10;
-        }
-        i1 = i;
-        while(i1>0){
-            int d = i1%10;
-            s+=pow(d, c);
-            i1/=10;
-        }
-        if(i == s)
+        if(isArmstrong(i))
             cout<<i<<" ";
     }
-    
 }
