58e5f8b32923a7768dbbc73fabd6a193368fc139
Devansh Gupta <78715794+DevanshGupta-6@users.noreply.github.com>
2026-06-02 20:34:57 +0545

    Create RecursiveFibonacci.cpp

---
+++ Day-7/RecursiveFibonacci.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int fibonacci(int n){
+   int a = 0, b = 1, c = 1;
+   if(c == n)
+     return b;
+   else{
+     
+   }
+}
+int main(){
+   int n;
+   cout<<"Enter a number: ";
+   cin>>n;
+   for(int i = 1; i<=n; i++){
+      
+   }
+}
