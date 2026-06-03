355dab0f22f9e5701c77a4f8153ff88fd864743a
Devansh Gupta <78715794+DevanshGupta-6@users.noreply.github.com>
2026-06-03 13:40:06 +0545

    Update recursiveReverse.cpp
    Reverses a number

--- Day-7/recursiveReverse.cpp
+++ Day-7/recursiveReverse.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 using namespace std;
-int reverse(int n){
+int reverse(int n, int rev){
    if(n == 0)
-    return 0;
+    return rev;
    else
-    return 
+    return reverse(n/10, (rev*10)+n%10);
+}
+int main(){
+   int n;
+   cout<<"Enter a number: ";
+   cin>>n;
+   cout<<reverse(n, 0);
 }
