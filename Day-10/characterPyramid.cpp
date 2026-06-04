13e7e4a1f224e141c4397a50d4817992a5576003
Devansh Gupta <78715794+DevanshGupta-6@users.noreply.github.com>
2026-06-04 20:07:40 +0545

    Create characterPyramid.cpp
    Code is incomplete

---
+++ Day-10/characterPyramid.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+   int base = 0;
+   for(int i = 1; i<=5; i++){
+      
+      for(int k = i; k<=4; k++){
+         cout<<" ";
+      }
+      
+      cout<<endl;
+   }
+}
