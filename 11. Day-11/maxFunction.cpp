d2c0bea364cfee0266fdbe6023b8afe4bffa877d
Devansh Gupta <78715794+DevanshGupta-6@users.noreply.github.com>
2026-06-04 20:16:47 +0545

    Create maxFunction.cpp

---
+++ 11. Day-11/maxFunction.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int maximum(int a, int b){
+   if(a>=b)
+    return a;
+   else
+    return b;
+}
+int main(){
+   int a, b;
+   cout<<"Enter two numbers: ";
+   cin>>a>>b;
+   cout<<maximum(a,b);
+}
