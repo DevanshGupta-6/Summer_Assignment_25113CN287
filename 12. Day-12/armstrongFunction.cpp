075bac7582d1df64eb1b8abcd7ace38cd2f02c84
Devansh Gupta <78715794+DevanshGupta-6@users.noreply.github.com>
2026-06-04 20:30:35 +0545

    Create 12. Day-12/armstrongFunction.cpp

---
+++ 12. Day-12/armstrongFunction.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath>
+bool isArmstrong(int n){
+    int n1 = n, s = 0, c = 0;
+    while(n>0){
+        c++;
+        n/=10;
+    }
+    n = n1;
+    while(n>0){
+        int d = n%10;
+        s+=pow(d, c);
+        n/=10;
+    }
+    if(n1 == s)
+     return true;
+    else
+     return false;
+}
+int main(){
+   int n;
+   cout<<"Enter a number: ";
+   cin>>n;
+   if(isArmstrong(n))
+    cout<<"It is an Armstrong number";
+   else
+    cout<<"It is not an Armstrong number";
+}
