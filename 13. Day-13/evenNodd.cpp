7725c302456c5276ce6aa34a1587647c5ca0c49f
Devansh Gupta <78715794+DevanshGupta-6@users.noreply.github.com>
2026-06-04 21:15:29 +0545

    Create evenNodd.cpp

---
+++ 13. Day-13/evenNodd.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main(){
+   int n;
+   cout<<"Enter number of terms: ";
+   cin>>n;
+   int arr[n];
+   cout<<"Enter numbers: ";
+   for(int i = 0; i<n; i++){
+      cin>>arr[i];
+   }
+   int even = 0, odd = 0;
+   for(int i = 0; i<n; i++){
+      if(arr[i]%2 == 0)
+       even++;
+      else
+       odd++;
+   }
+   cout<<"There are "<<even<<" even elements and "<<odd<<" odd elements";
+}
