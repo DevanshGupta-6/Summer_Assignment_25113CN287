196b921a56db30a2fe3e3497fccc6a0dce7ce64d
DevanshGupta-6 <contest112010@gmail.com>
2026-06-06 18:48:48 +0530

    zeroes moved to end

---
+++ 15_Day-15/zeroToEnd.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+   int n;
+   cout<<"Enter number of terms: ";
+   cin>>n;
+   vector<int> arr(n);
+   cout<<"Enter numbers: ";
+   for(int i = 0; i<n; i++){
+      cin>>arr[i];
+   }
+   vector<int> newarr(n);
+   int newIndex = 0;
+   for(int i = 0; i<n; i++){
+    if(arr[i] == 0)
+     continue;
+    newarr[newIndex] = arr[i];
+    newIndex++;
+   }
+   for(int i = 0; i<n; i++){
+    cout<<newarr[i]<<" ";
+   }
+}
