76802e34e6dc55c448627e67d02bbdb73fe4ef6a
DevanshGupta-6 <contest112010@gmail.com>
2026-06-07 13:18:57 +0530

    Displays no. with max Freq

--- P_Day-16/maximumFrequency.cpp
+++ P_Day-16/maximumFrequency.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 using namespace std;
+int mostFreqEle(vector<int> &arr){
+  int n = arr.size();
+  unordered_map<int, int> freq;
+  for(int i = 0; i<n; i++){
+    freq[arr[i]]++;
+  }
+  int maxCnt = 0, res = -1;
+  for(auto i : freq){
+    int val = i.first, cnt = i.second;
+
+    if(maxCnt<cnt || (cnt == maxCnt && val>res)){
+      res = val;
+      maxCnt = cnt;
+    }
+  }
+  return res;
+}
 int main(){
     int n;
     cout<<"Enter number of terms: ";
@@ -10,5 +28,5 @@ int main(){
     for(int i = 0; i<n; i++){
       cin>>arr[i];
     }
-    
+    cout<< mostFreqEle(arr)<<" is mentioned most of the times.";
 }
