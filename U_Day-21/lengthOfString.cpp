b68c6a98e5249ba1efde531764b0fabaaeb63fad
DevanshGupta-6 <contest112010@gmail.com>
2026-06-09 22:04:40 +0530

    Removed length() used by mistake

--- U_Day-21/lengthOfString.cpp
+++ U_Day-21/lengthOfString.cpp
@@ -6,8 +6,11 @@ int main(){
     getline(cin, s);
 
     int sum = 0;
-    for(int i = 0; i<s.length(); i++){
+    int i = 0;
+    while(s[i] != '\0'){
         sum++;
+        i++;
     }
+    
     cout<<sum<<" is th length of the string";
 }
