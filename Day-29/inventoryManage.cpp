7eb2980eb6d001442e39a21519b033e9e944e03c
DevanshGupta-6 <contest112010@gmail.com>
2026-06-21 19:28:06 +0530

    added search

--- Day-29/inventoryManage.cpp
+++ Day-29/inventoryManage.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 using namespace std;
+int search(string items[], int quantity[], int n, string key){
+    for(int i=0;i<n;i++){
+        if(items[i]==key){
+            return quantity[i];
+        }
+    }
+    return -1;
+}
 int main(){
     int n;
     cout<<"Enter the number of items in the inventory: ";
@@ -16,5 +24,10 @@ int main(){
     for(int i=0;i<n;i++){
         cout<<"Item: "<<items[i]<<" | Quantity: "<<quantity[i]<<endl;
     }
+    cout<<"Enter the name of the item to search: ";
+    string key;
+    cin>>key;
+    int result = search(items, quantity, n, key);
+    cout<<"The quantity of "<<key<<" is: "<<result<<endl;
     return 0;
 }
